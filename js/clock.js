40d8e4a3473e9a02b70285995eb1b53b26968db3
Zippy Boy <106349143+kingman11211@users.noreply.github.com>
2022-12-16 17:24:53 -0500

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,24 @@
-<iframe src="https://free.timeanddate.com/clock/i8mx6mxu/n25/th2" frameborder="0" width="113" height="18"></iframe>
+function currentTime() {
+        var date = new Date(); /* creating object of Date class */
+        var hour = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+        hour = updateTime(hour);
+        min = updateTime(min);
+        sec = updateTime(sec);
+        document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+        var t = setTimeout(function() {
+          currentTime()
+        }, 1000); /* setting timer */
+      }
+
+      function updateTime(k) {
+        if (k < 10) {
+          return "0" + k;
+        } else if (k > 12) {
+          return k - 12;
+        } else {
+        return k;
+        }
+      }
+      currentTime(); /* calling currentTime() function to initiate the process */
