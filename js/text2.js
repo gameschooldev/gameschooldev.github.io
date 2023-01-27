ed56bcd7fdc9278bd2b4267346c508528d4c002b
Zippy Boy <106349143+kingman11211@users.noreply.github.com>
2023-01-26 19:19:39 -0500

    Update text2.js

--- js/text2.js
+++ js/text2.js
@@ -1,4 +1,5 @@
 const carouselText = [
+  {text: "(1/26/23) Finally removed world cup from all the pages. Next update comes the proxies😈", color: "rgb(0, 255, 200)"},
   {text: "(12/16/22) ADDED WORKING BLOOKET HACKS! GET MAX COINS!", color: "rgb(0, 255, 200)"},
   {text: "(12/16/22) Added Crossy Road and Doodle Jump but u all prob arnt gonna play bc ITS ALMOST CRISTMAS and we are mostly all on break so bye!", color: "rgb(0, 255, 200)"},
   {text: "(12/05/22) Added 7 new games, bypasses.html, and request.html. Give me some games!", color: "rgb(0, 255, 200)"},
