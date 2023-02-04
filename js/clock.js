820b7361cad26fa1d50b710e5817872fc0ed603e
gameschooldev <119633310+gameschooldev@users.noreply.github.com>
2023-02-04 10:57:47 -0500

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,24 +1,29 @@
-function currentTime() {
-        var date = new Date(); /* creating object of Date class */
-        var hour = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-        hour = updateTime(hour);
-        min = updateTime(min);
-        sec = updateTime(sec);
-        document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
-        var t = setTimeout(function() {
-          currentTime()
-        }, 1000); /* setting timer */
-      }
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
 
-      function updateTime(k) {
-        if (k < 10) {
-          return "0" + k;
-        } else if (k > 12) {
-          return k - 12;
-        } else {
-        return k;
-        }
-      }
-      currentTime(); /* calling currentTime() function to initiate the process */
+showTime();
