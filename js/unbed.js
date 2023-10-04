bac95675e891dc4f321a2dec4c9afa749b2c4cd6
gameschooldev <119633310+gameschooldev@users.noreply.github.com>
2023-10-03 23:27:20 -0400

    Update unbed.js

--- js/unbed.js
+++ js/unbed.js
@@ -2,8 +2,8 @@
 if(window.self != window.top) {
 //Analyzing URL to check embed link
 try {
-if(window.parent.location.hostname.indexOf("https://example.com") == -1) {
-window.location.href = "https://example.com";
+if(window.parent.location.hostname.indexOf("https://kings-schoolwork.vercel.app/Pages/games.html, https://kingman11211-git-main-gameschooldev.vercel.app/Pages/games.html, https://kingman11211-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-git-main-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-gameschooldev.vercel.app/Pages/games.html") == -1) {
+window.location.href = "https://kings-schoolwork.vercel.app/Pages/games.html, https://kingman11211-git-main-gameschooldev.vercel.app/Pages/games.html, https://kingman11211-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-git-main-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-gameschooldev.vercel.app/Pages/games.html";
 } else {
 //iFrame from host site
 }
