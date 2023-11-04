b48dc8216c5f3977cccf5f2e998b1cfa9d2d89c9
Zippy Boy <106349143+kingman11211@users.noreply.github.com>
2023-11-04 12:45:57 -0400

    Update unbed.js

--- js/unbed.js
+++ js/unbed.js
@@ -1,14 +1,1 @@
-//Checking if the page is loaded in an iframe
-if(window.self != window.top) {
-//Analyzing URL to check embed link
-try {
-if(window.parent.location.hostname.indexOf("https://kings-schoolwork.vercel.app/Pages/games.html, https://kingman11211-git-main-gameschooldev.vercel.app/Pages/games.html, https://kingman11211-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-git-main-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-gameschooldev.vercel.app/Pages/games.html") == -1) {
-window.location.href = "https://kings-schoolwork.vercel.app/Pages/games.html, https://kingman11211-git-main-gameschooldev.vercel.app/Pages/games.html, https://kingman11211-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-git-main-gameschooldev.vercel.app/Pages/games.html, https://kings-schoolwork-gameschooldev.vercel.app/Pages/games.html";
-} else {
-//iFrame from host site
-}
-} catch (ex) {
-//Blocking Embed...
-window.location.href = "https://gameschooldev.github.io/skid-site/";
-}
-} 
+
